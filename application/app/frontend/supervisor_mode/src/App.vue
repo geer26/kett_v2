e1d7a4114e9dcbaed83a4703ee216445a2ca9a47
geer26 <gergo.kurucz@gmail.com>
2023-05-01 17:02:03 +0200

    0.1

--- application/app/frontend/supervisor_mode/src/App.vue
+++ application/app/frontend/supervisor_mode/src/App.vue
@@ -1,14 +1,23 @@
 <template>
 
-  <h1>HELLO WORLD</h1>
+  <Room_selector v-if="!this.room"/>
+
+  <div></div>
+
 </template>
 
 <script>
 
+import Room_selector from './components/Room_selector_component.vue'
+
 export default {
   name: 'App',
   components: {
-  }
+    Room_selector
+  },
+  data(){return{
+    room: null,
+  }}
 }
 </script>
 
