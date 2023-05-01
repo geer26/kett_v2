142e82cd8e9e901cfec6364ed4cae23260a48c00
geer26 <gergo.kurucz@gmail.com>
2023-05-01 15:33:46 +0200

    0.1

---
+++ application/app/frontend/supervisor_mode/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+
+  <h1>HELLO WORLD</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ application/app/frontend/supervisor_mode/vue.config.js
@@ -0,0 +1,31 @@
+const { defineConfig } = require('@vue/cli-service')
+
+//PRODUCTION SETTING
+//UNCOMMENT ON COMPILE
+/*
+module.exports = defineConfig({
+    publicPath: '../../static/supervisor_dist',
+    outputDir: '../../static/supervisor_dist',
+    indexPath: '../../templates/supervisor_vue.html',
+
+    configureWebpack: {
+      devServer: {
+        devMiddleware: {
+            writeToDisk: true
+        }
+      }
+    },
+
+    transpileDependencies: true
+})
+*/
+
+
+//DEV SETTING
+//UNCOMMENT AT DEV
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  outputDir: "./dist/",
+  assetsDir: "static",
+})
