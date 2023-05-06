9999d174fbdb02584c2b102eadeadc0f574d65d9
geer26 <gergo.kurucz@gmail.com>
2023-05-06 17:08:39 +0200

    connection modal

--- application/app/frontend/supervisor_mode/src/App.vue
+++ application/app/frontend/supervisor_mode/src/App.vue
@@ -6,10 +6,18 @@
 
   <div class="main_container">
 
-    <div class="station_table_container">
+    <div class="table_label">
+
+      <img v-if="this.conn" src="./assets/img/connected.png" alt="" class="connectionimage">
+      <img v-if="!this.conn" src="./assets/img/disconnected.png" alt="" class="connectionimage">
       STATION TABLE
-      <ConnectionState/>
       <ConnectionManager/>
+
+    </div>
+
+    <div class="station_table_container glassmorphism_gray">
+
+        
     </div>
 
     <div class="control_panel_container">
@@ -26,10 +34,6 @@
 
       <div class="timer_container"><p class="red_text">TIME</p></div>
 
-      
-      
-      
-
     </div>
   </div>
 
@@ -39,7 +43,7 @@
 
 import Room_selector from './components/Room_selector_component.vue'
 import Spinner_component from "./components/Spinner_component.vue"
-import ConnectionState from './components/Connectionstate.vue'
+//import ConnectionState from './components/Connectionstate.vue'
 import ConnectionManager from './components/ConnectionManager.vue'
 import { socket, state } from "@/socket"
 
@@ -50,8 +54,14 @@ export default {
   components: {
     Room_selector,
     Spinner_component,
-    ConnectionState,
+    //ConnectionState,
     ConnectionManager,
+  },
+
+  computed: {
+    conn() {
+      return state.connected
+    }
   },
 
   methods: {
@@ -78,19 +88,14 @@ export default {
 
 .main_container{
   position: relative;
-  height: 99%;
+  height: 100%;
   width: 99%;
-  
-  display: grid;
+  display:flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
 
-  grid-template-columns: 1fr;
-  grid-template-rows: 6fr 1fr;
-
-  justify-content: space-evenly;
-  grid-template-areas:
-        "station_table"
-        "control_panel";
-  
+  overflow: hidden;
 }
 
 .spinner{
@@ -107,27 +112,34 @@ export default {
 }
 
 .station_table_container{
-  grid-area: station_table;
-  border: 2px solid red;
   position: relative;
+  height: 75%;
+  width: 99%;
   padding: 20px;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: flex-start;
+  overflow: auto;
+}
+
+.table_label{
+  position: relative;
+  height: 15%;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
 }
 
 .control_panel_container{
   grid-area: control_panel;
+  height: 10%;
+  width: 100%;
   position: relative;
   padding-left: 20px;
   padding-right: 20px;
-  /*
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  */
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   grid-template-rows: 1fr;
@@ -137,7 +149,6 @@ export default {
 }
 
 .select_workout_container{
-  border: 2px solid red;
   grid-area: select_workout;
   display: flex;
   flex-direction: row;
@@ -160,7 +171,6 @@ export default {
 }
 
 .start_button_container{
-  border: 2px solid red;
   grid_area: start_button;
   display: flex;
   flex-direction: row;
@@ -169,12 +179,15 @@ export default {
 }
 
 .timer_container{
-  border: 2px solid red;
   grid-area: timer;
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
+}
+
+.connectionimage {
+  max-height: 30%;
 }
 
 </style>
